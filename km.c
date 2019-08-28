96ba6cdf28c6e569b1be1266a7ad0db12ebfc355
castrated <47220664+castrated@users.noreply.github.com>
2019-08-28 14:15:47 -0400

    Create km.c
    workin on it

---
+++ km.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <termios.h>
+struct termios orig_termios;
+void disableRawMode(){
+  tcsetattr(STDIN_FILENO,TCSAFLUSH,&orig_termios);
+}
+void enableRawMode(){
+  struct termios raw = orig_termios;
+  atexit(disableRawMode);
+  tcgetattr(STDIN_FILENO,&raw);
+  raw.c_lflag&=~(ECHO | ICANON);
+  tcsetattr(STDIN_FILENO,TCSAFLUSH,&raw);
+}
+int main(){
+  enableRawMode();
+  char c;
+  while(read(STDIN_FILENO,&c,1)==1&&c!='q'); //q to quit
+  return(0);
+}
