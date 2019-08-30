4953bf09a5077b77fdb04dfee3affa2c12e0535b
castrated <47220664+castrated@users.noreply.github.com>
2019-08-30 12:57:22 -0400

    Update km.c

--- km.c
+++ km.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <ctype.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <termios.h>
@@ -16,6 +17,12 @@ void enableRawMode(){
 int main(){
   enableRawMode();
   char c;
-  while(read(STDIN_FILENO,&c,1)==1&&c!='q'); //q to quit
+  while(read(STDIN_FILENO,&c,1)==1&&c!='q'){
+    if(iscntrl(c)){
+      printf("%d\n",c);
+    }else{
+      printf("%d('%c')\n",c,c);
+    }
+  }//q to quit
   return(0);
 }
